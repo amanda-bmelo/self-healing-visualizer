9046e649a2bc23031a8d8f491dc7466a916b5089
Bruno Rodrigues <brunoarmm@id.uff.br>
2023-10-10 01:18:56 -0300

    test: test now skips turns for fixing fault

--- self_healing_visualizer/interface/run.py
+++ self_healing_visualizer/interface/run.py
@@ -1,6 +1,6 @@
 from time import sleep
 from self_healing_visualizer.devices_interface.mainUI import MainUI
-from self_healing_visualizer.case_study2 import *
+from self_healing_visualizer.case_study3 import *
 from self_healing_visualizer.util.global_clock import GlobalClock
 
 def update(mui, delay):
@@ -16,15 +16,17 @@ def run_UI(sub_ts: list["CaseStudy"]):
     mui.run()
     for wave in sub_ts[0].faulty_wires_waves:
         GlobalClock.run_all_batches(
-            lambda gc: update(mui, 1)
+            lambda gc: 0
         )
         mui.run()
-        sleep(1)
+        sleep(1.3)
         for faulty_wire in wave:
             y,x = faulty_wire
             sub_ts[0].matrix[y][x].fault(None)
+        mui.run()
+        sleep(1.3)
         GlobalClock.run_all_batches(
-            lambda gc: update(mui, 1)
+            lambda gc: 0
         )
         mui.run()
         sleep(1)
